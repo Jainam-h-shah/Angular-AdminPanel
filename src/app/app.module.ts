5ef76d8381fa4bf55cc8e143395ba173e6caa36c
Jainam-h-shah <sjainam82@gmail.com>
2023-04-19 22:14:33 +0530

    add-change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BadgeModule, IconModule, SidebarModule } from 'ng-cdbangular';
 import { Page404Component } from './page404/page404.component';
 import { AuthServiceService } from './service/auth-service.service';
-
+ 
 @NgModule({
   declarations: [
     AppComponent,
